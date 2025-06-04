2bc10cfd16610f75558644f5a02f4439b8d734f9
Chinedum Nnaji <213635443+programmerforchrist@users.noreply.github.com>
2025-06-04 09:28:54 +0100

    Edit site title and add functions for human/computer choice

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Jesus is Lord');
+function getComputerChoice () {
+    let choice = Math.random() * 15
+    if(choice <= 5) {
+        return "Paper";
+    } else if(choice <= 10) {
+        return "Scissors";
+    } else {
+        return "Rock";
+    }    
+}
+
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+    let humanChoice = prompt("Enter your Choice:");
+    return humanChoice;
+}
+
+console.log(getHumanChoice());
